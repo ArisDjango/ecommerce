f30300613723afe2258f5674d509d12d629110d7
aris winandi <winandiaris@gmail.com>
2020-10-30 16:18:15 +0700

    checkout page

--- ecommercePy/store/views.py
+++ ecommercePy/store/views.py
@@ -1,14 +1,41 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+import json
+import datetime
+from .models import *
+#from .utils import cookieCart, cartData, guestOrder
 # Create your views here.
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {'products':products}
     return render(request, 'store/store.html', context)
 
 def cart(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_item':0}
+
+    context = {
+        'items':items,
+        'order':order,
+    }
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_item': 0}
+
+    context = {
+        'items': items,
+        'order': order,
+    }
     return render(request, 'store/checkout.html', context)
